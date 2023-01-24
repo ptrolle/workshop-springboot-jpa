0f5808a7c0c1ce4344560807f4976bf89b1c9453
Pablo Trolle <pablotrolle@yahoo.com>
2023-01-23 22:47:38 -0300

    Project Created

---
+++ src/main/java/com/ptrolle/newDateBaseTest/NewDateBaseTestApplication.java
@@ -0,0 +1,13 @@
+package com.ptrolle.newDateBaseTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewDateBaseTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewDateBaseTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ptrolle/newDateBaseTest/NewDateBaseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptrolle.newDateBaseTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewDateBaseTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
