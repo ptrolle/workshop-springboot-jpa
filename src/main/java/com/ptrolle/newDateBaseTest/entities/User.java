792d7c53c67d390dc34adb8f9a51932c440fe9af
Pablo Trolle <pablotrolle@yahoo.com>
2023-01-24 15:16:16 -0300

    H2 database, test profile, JPA

--- src/main/java/com/ptrolle/newDateBaseTest/entities/User.java
+++ src/main/java/com/ptrolle/newDateBaseTest/entities/User.java
@@ -1,12 +1,17 @@
 package com.ptrolle.newDateBaseTest.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user") //usamos isso para renomear para nao dar conflito com a palavra reservada no banco H2 User
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //para auto incremento de id no banco de dados
     private Long id;
     private String name;
     private String email;
