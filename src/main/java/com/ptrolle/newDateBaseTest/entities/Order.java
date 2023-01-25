f61a23e204cde51b0d58bb37883760002e9ef95f
Pablo Trolle <pablotrolle@yahoo.com>
2023-01-25 14:58:10 -0300

    Created Project

---
+++ src/main/java/com/ptrolle/newDateBaseTest/entities/Order.java
@@ -0,0 +1,10 @@
+package com.ptrolle.newDateBaseTest.entities;
+
+import java.time.Instant;
+
+public class Order {
+
+    private Long id;
+    private Instant moment; //Instance é parecido com a class Date mas mais aprimorada
+
+}
